3615979998ff4fba814fff48891216913a217f11
csh-iaas-cec <ravi.devarakonda@oracle.com>
2020-06-02 21:37:35 +0530

    Cloud native config

--- Config.py
+++ Config.py
@@ -3,20 +3,20 @@ import configparser
 
 
 # get the cloud shell delegated authentication token
-# delegation_token = open('/etc/oci/delegation_token', 'r').read()
+delegation_token = open('/etc/oci/delegation_token', 'r').read()
 
-# configParser = configparser.RawConfigParser()   
-# configFilePath = r'/etc/oci/config'
-# configParser.read(configFilePath)
+configParser = configparser.RawConfigParser()   
+configFilePath = r'/etc/oci/config'
+configParser.read(configFilePath)
 
-# tenancy_id = configParser.get('us-ashburn-1', 'tenancy')
+tenancy_id = configParser.get('us-ashburn-1', 'tenancy')
 
 
 
 
 # # create the api request signer
-# signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner(
-#    delegation_token=delegation_token
-# )
-from oci.config import from_file
-config = from_file('~/.oci/config',"red")
+signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner(
+   delegation_token=delegation_token
+)
+# from oci.config import from_file
+# config = from_file('~/.oci/config',"red")
