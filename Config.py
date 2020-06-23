17de202068c4cac5990d32d8549fbd1bd63dc293
csh-iaas-cec <ravi.devarakonda@oracle.com>
2020-06-23 15:20:09 +0530

    cleaned config

--- Config.py
+++ Config.py
@@ -2,21 +2,5 @@ import oci
 import configparser
 
 
-# get the cloud shell delegated authentication token
-# delegation_token = open('/etc/oci/delegation_token', 'r').read()
-
-# configParser = configparser.RawConfigParser()   
-# configFilePath = r'/etc/oci/config'
-# configParser.read(configFilePath)
-
-# tenancy_id = configParser.get('us-ashburn-1', 'tenancy')
-
-
-
-
-# # create the api request signer
-# signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner(
-#    delegation_token=delegation_token
-# )
 from oci.config import from_file
 config = from_file('~/.oci/config',"red")
