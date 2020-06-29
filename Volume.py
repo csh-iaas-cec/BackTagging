a81a5678bc6a4200fae724bab37a2830fb5d8607
csh-iaas-cec <ravi.devarakonda@oracle.com>
2020-06-29 21:44:14 +0530

    updated finally

--- Volume.py
+++ Volume.py
@@ -36,8 +36,10 @@ class Volume:
             volume_detail = self.block_storage_client.get_volume(volume_id).data
             logging.info("KIshore")
             tags = volume_detail.defined_tags
-            tags["Block-Storage-tags"]["InstanceName"] = tag["InstanceName"]
-            tags["Block-Storage-tags"]["VSAD"] = tag["VSAD"]
+            temp = dict()
+            temp["InstanceName"] = tag["InstanceName"]
+            temp["VSAD"] = tag["VSAD"]
+            tags["Block-Storage-tags"] = temp
             logging.info(volume_detail.defined_tags)
 
             volume_details = oci.core.models.UpdateVolumeDetails(
@@ -62,8 +64,10 @@ class Volume:
         try:
             volume_detail = self.block_storage_client.get_volume_backup(volume_backup_id).data
             tags = volume_detail.defined_tags
-            tags["Block-Storage-tags"]["InstanceName"] = tag["InstanceName"]
-            tags["Block-Storage-tags"]["VSAD"] = tag["VSAD"]
+            temp = dict()
+            temp["InstanceName"] = tag["InstanceName"]
+            temp["VSAD"] = tag["VSAD"]
+            tags["Block-Storage-tags"] = temp
             volume_backup_details = oci.core.models.UpdateVolumeBackupDetails(
                 defined_tags=tags
             )
