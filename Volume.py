51a51d9ade151186073b5b263ae80aaf33cefef2
csh-iaas-cec <ravi.devarakonda@oracle.com>
2020-06-29 21:34:33 +0530

    update

--- Volume.py
+++ Volume.py
@@ -32,19 +32,23 @@ class Volume:
     # Request to update the volume tags
     def update_volume_tag(self, volume_id, tag):
         try:
+            
             volume_detail = self.block_storage_client.get_volume(volume_id).data
+            logging.info("KIshore")
             tags = volume_detail.defined_tags
             tags["Block-Storage-tags"]["InstanceName"] = tag["InstanceName"]
             tags["Block-Storage-tags"]["VSAD"] = tag["VSAD"]
-        
+            logging.info(volume_detail.defined_tags)
+
             volume_details = oci.core.models.UpdateVolumeDetails(
                 defined_tags=tags
             )
             self.block_storage_client.update_volume(volume_id, volume_details)
             logging.info("Updated Volume tags", volume_id)
-        except KeyError:
+        except KeyError as e:
             if(volume_detail.lifecycle_state == "TERMINATED"):
                 logging.warning("Already terminated "+" "+volume_id)
+            logging.error(e)
         except oci.exceptions.ServiceError as identifier:
             if(identifier.status == 400):
                 logging.warning(volume_id+" already updated")
@@ -67,9 +71,10 @@ class Volume:
                 volume_backup_id, volume_backup_details
             )
             logging.info("Updated Volume Backup tags"+" "+ volume_backup_id)
-        except KeyError:
+        except KeyError as e:
             if(volume_detail.lifecycle_state == "TERMINATED"):
                 logging.warning("Already terminated "+" "+ volume_backup_id)
+            logging.error(e)
         except oci.exceptions.ServiceError as identifier:
             if(identifier.status == 400):
                 logging.warning(volume_backup_id+" already updated")
